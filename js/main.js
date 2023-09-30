18cd20a8bf4d2c70c53a6c92b84da6b2cf335404
CALLUM BORWELL <callum.borwell@gmail.com>
2023-09-30 23:32:36 +1000

    test commit to a private git

--- js/main.js
+++ js/main.js
@@ -1,19 +1,114 @@
 // the game itself
 var game;
+var hordeCount = 0;
 
 var gameOptions = {
 
     // slices (prizes) placed in the wheel
-    slices: 6,
+    slices: 100,
 
     // prize names, starting from 12 o'clock going clockwise
     slicePrizes: [
-        "🎉 5% OFF",
-        "🎉 10% OFF",
-        "🎉 15% OFF",
-        "🎉 25% OFF",
-        "🎉 50% OFF",
-        "🎉 FREE PASTRY 🍰"
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "Goblin",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a Trenchcoat",
+        "2 Goblins in a  Trenchcoat",
+        "Three Goblins in a Trenchcoat",
+        "Three Goblins in a Trenchcoat",
+        "Three Goblins in a Trenchcoat",
+        "Three Goblins in a Trenchcoat",
+        "Three Goblins in a Trenchcoat",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Goblin Archer",
+        "Uncommon Magic Item",
+        "Uncommon Magic Item",
+        "Uncommon Magic Item",
+        "Uncommon Magic Item",
+        "Uncommon Magic Item",
+        "Uncommon Magic Item",
+        "Uncommon Magic Item",
+        "Uncommon Magic Item",
+        "Uncommon Magic Item",
+        "Uncommon Magic Item",
+        "Rare Magic Item",
+        "Rare Magic Item",
+        "Rare Magic Item",
+        "Rare Magic Item",
+        "Rare Magic Item",
+        "Goblin Brute",
+        "Goblin Brute",
+        "Goblin Brute",
+        "Goblin Brute",
+        "Goblin Brute",
+        "Warg Rider",
+        "Warg Rider",
+        "Warg Rider",
+        "Goblin Mage",
+        "Goblin Mage"
     ],
 
     // wheel rotation duration, in milliseconds
@@ -36,7 +131,7 @@ window.onload = function () {
         height: 850,
 
         // game background color
-        backgroundColor: 0x880044,
+        backgroundColor: '#000000',//0x880044,
 
         // scenes used by the game
         scene: [playGame]
@@ -49,6 +144,28 @@ window.onload = function () {
     window.focus()
     resize();
     window.addEventListener("resize", resize, false);
+
+    // visibility
+    document.getElementById("popUpList").style.visibility = "hidden";
+    document.getElementById("popUpFail").style.visibility = "hidden";
+    document.getElementById("horde").style.visibility = "hidden";
+    document.getElementById("hide").style.visibility = "hidden";
+
+    // disable all buttons
+    const buttons = document.getElementsByTagName("button"); for (let button of buttons) { button.disabled = true; }
+    //game.setSpin(false);
+
+    // Control buttons on based on if donor name is blank
+    document.getElementById("donorName").addEventListener("keyup", (event) => {
+        if (event.target.value == "") {
+            for (let button of buttons) { button.disabled = true; }
+            //game.setSpin(false);
+        } else {
+            for (let button of buttons) { button.disabled = false; }
+            //game.setSpin(true);
+        }
+    });
+
 }
 
 // PlayGame scene
@@ -62,8 +179,8 @@ class playGame extends Phaser.Scene {
     // method to be executed when the scene preloads
     preload() { // loading assets
 
-        this.load.image("wheel", window.location.href + "images/wheel.png");
-        this.load.image("pin", window.location.href + "images/pin.png");
+        this.load.image("wheel", "https://i.imgur.com/CnX4XV8.png");
+        this.load.image("pin", "https://i.imgur.com/Tmh3Hgn.png");
     }
 
     // method to be executed once the scene has been created
@@ -76,10 +193,10 @@ class playGame extends Phaser.Scene {
         this.pin = this.add.sprite(game.config.width / 2, game.config.height / 2, "pin");
 
         // adding the text field
-        this.prizeText = this.add.text(game.config.width / 2, game.config.height - 35, "SPIN TO WIN", {
+        this.prizeText = this.add.text(game.config.width / 2, game.config.height - 35, "", {
             font: "bold 64px Rajdhani",
             align: "center",
-            color: "white"
+            color: "black"
         });
 
         // center the text
@@ -90,6 +207,10 @@ class playGame extends Phaser.Scene {
 
         // waiting for your input, then calling "spinWheel" function
         this.input.on("pointerdown", this.spinWheel, this);
+    }
+
+    setSpin(bool) {
+        this.canSpin = bool;
     }
 
     // function to spin the wheel
@@ -137,8 +258,26 @@ class playGame extends Phaser.Scene {
                     // displaying prize text
                     this.prizeText.setText(gameOptions.slicePrizes[prize]);
 
+                    // add prize to horde
+                    switch(gameOptions.slicePrizes[prize]) {
+                        case "2 Goblins in a Trenchcoat":
+                            goblintrenchcoatAdd(2);
+                            break;
+                        case "Three Goblins in a Trenchcoat":
+                            goblintrenchcoatAdd(3);
+                            break;
+                        case "Uncommon Magic Item":
+                            goblinitemAdd("Uncommon Item");
+                            break;
+                        case "Rare Magic Item":
+                            goblinitemAdd("Rare Item");
+                            break;
+                        default:
+                            goblinAdd(gameOptions.slicePrizes[prize]);
+                      }
+
                     // player can spin again
-                    this.canSpin = false;
+                    this.canSpin = true;
                 }
             });
         }
@@ -148,8 +287,8 @@ class playGame extends Phaser.Scene {
 // pure javascript to scale the game
 function resize() {
     var canvas = document.querySelector("canvas");
-    var windowWidth = window.innerWidth;
-    var windowHeight = window.innerHeight;
+    var windowWidth = 500;
+    var windowHeight = 500;
     var windowRatio = windowWidth / windowHeight;
     var gameRatio = game.config.width / game.config.height;
     if (windowRatio < gameRatio) {
@@ -159,5 +298,143 @@ function resize() {
     else {
         canvas.style.width = (windowHeight * gameRatio) + "px";
         canvas.style.height = windowHeight + "px";
+    }
+}
+
+// add goblin to list
+function goblinAdd(goblinType) {
+    var text = document.getElementById("donorName").value; 
+    var li = "<li id='g"+hordeCount+"' onclick=\"death('g"+hordeCount+"')\">" + goblinType + " " + text + "</li>";
+    document.getElementById("list").innerHTML += li;
+    hordeCount++;
+    goblinCounter(hordeCount);
+    fumbleCheck(goblinType);
+    document.getElementById("donorName").value = "";
+}
+
+// add goblin trenchcoats to list
+function goblintrenchcoatAdd(goblinCount) {
+    var text = document.getElementById("donorName").value; 
+    var li = "<li id='g"+hordeCount+"' style='display:inline' onclick=\"death('g"+hordeCount+"')\">" + goblinCount + " Goblins " + text; li += "</li>";
+    for (let i = 0; i < goblinCount; i++) {
+        li += "<input type='checkbox'>";
+    }
+    li += "<br>"
+    document.getElementById("list").innerHTML += li;
+    hordeCount++;
+    goblinCounter(hordeCount);
+    fumbleCheck(goblinCount+" Goblins");
+    document.getElementById("donorName").value = "";
+}
+
+// add goblin items to list
+function goblinitemAdd(goblinRarity) {
+    var text = document.getElementById("donorName").value; 
+    var li = "<li id='g"+hordeCount+"' style='display:inline' onclick=\"death('g"+hordeCount+"')\">Goblin " + text + " with </li><input type='text' value='"+goblinRarity+"'>";
+    li += "<br>"
+    document.getElementById("list").innerHTML += li;
+    hordeCount++;
+    goblinCounter(hordeCount);
+    fumbleCheck(goblinRarity+" Goblin");
+    document.getElementById("donorName").value = "";
+}
+
+// add death css
+function death(idValue) {
+    document.getElementById(idValue).classList.add('death');
+}
+
+// set goblin Count
+function goblinCounter(count) {
+    document.getElementById("hordeCount").innerHTML = count;
+    setFumblePrompt();
+}
+
+// check horde
+function checkHorde() {
+    document.getElementById("hordeTitle").innerHTML = "Growing Horde";
+    document.getElementById("popUpList").style.visibility = "visible";
+    document.getElementById("horde").style.visibility = "hidden";
+    document.getElementById("hide").style.visibility = "visible";
+}
+
+// hide horde
+function hideHorde() {
+    document.getElementById("popUpList").style.visibility = "hidden";
+    document.getElementById("horde").style.visibility = "hidden";
+    document.getElementById("hide").style.visibility = "hidden";
+}
+
+// show horde
+function showHorde() {
+    document.getElementById("popUpList").style.visibility = "visible";
+    document.getElementById("horde").style.visibility = "visible";
+    document.getElementById("hide").style.visibility = "hidden";
+}
+
+// change header to leader
+function hordeLeader(goblinType) {
+    var text = document.getElementById("donorName").value; 
+    document.getElementById("hordeTitle").innerHTML = goblinType + " " + text + "'s Horde!";
+    document.getElementById("hordeTitle").classList.remove("death");
+    document.getElementById("donorName").value = "";
+    hordeCount = 0;
+    goblinCounter(hordeCount);
+    document.getElementById("popUpList").style.visibility = "visible";
+    document.getElementById("horde").style.visibility = "visible";
+    document.getElementById("hide").style.visibility = "hidden";
+}
+
+// set fumblePrompt
+function setFumblePrompt() {
+    document.getElementById("fumblePrompt").innerHTML = "roll above "+hordeCount+" on a d100";
+}
+
+// fumble check
+function fumbleCheck(fumblerType) {
+    // perform roll animation
+
+    // roll d100
+    let x = Math.floor((Math.random() * 100) + 1);
+    document.getElementById("fumbleRoll").innerHTML = x;
+
+    // check if fumble
+    if (x >= hordeCount) {
+        document.getElementById("fumbleResult").innerHTML = "Fumble Check passed";
+    } else {
+        document.getElementById("fumbleResult").innerHTML = "Fumble Check FAILED!";
+        var text = document.getElementById("donorName").value;
+        document.getElementById("popUpFail").style.visibility = "visible";
+        document.getElementById("fumbleCulprit").innerHTML = fumblerType+" "+text+" stumbles into the open!";
+        document.getElementById("hordeTitle").innerHTML = fumblerType + " " + text + "'s Horde!";
+        document.getElementById("hordeTitle").classList.remove("death");
+    }
+}
+
+function fumbleHorde() {
+    document.getElementById("popUpFail").style.visibility = "hidden";
+    document.getElementById("popUpList").style.visibility = "visible";
+    document.getElementById("horde").style.visibility = "visible";
+    document.getElementById("hide").style.visibility = "hidden";
+}
+
+// end an encounter
+function confirmEnd() {
+    if (confirm('Are you sure you want to end this goblin horde?')) {
+        // delete the list
+        document.getElementById("list").innerHTML = "";
+
+        // reset the heading to growing horde
+        document.getElementById("hordeTitle").innerHTML = "Growing Horde";
+        document.getElementById("hordeTitle").classList.remove("death");
+
+        // set goblin horde count to 0
+        hordeCount = 0;
+        goblinCounter(hordeCount);
+
+        // invis the horde list window
+        hideHorde();
+    } else {
+        // Do nothing!
     }
 }
